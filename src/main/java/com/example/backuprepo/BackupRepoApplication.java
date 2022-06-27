9bcbc9300d2ba86f680cc6c7abcd5c2d01612619
User <lvmehriban@gmail.com>
2022-06-27 22:22:53 +0400

    Initial commit

---
+++ src/main/java/com/example/backuprepo/BackupRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.backuprepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackupRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackupRepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backuprepo/BackupRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backuprepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackupRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
